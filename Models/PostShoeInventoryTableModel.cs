26ca299cff0df317e24895043cdd916758225b0d
joelgoh15 <147377225+joelgoh15@users.noreply.github.com>
2023-11-22 17:21:54 +0800

    Update PostShoeInventoryTableModel.cs

--- Models/PostShoeInventoryTableModel.cs
+++ Models/PostShoeInventoryTableModel.cs
@@ -8,15 +8,9 @@ namespace Portfolio3_2.Models
     public class PostShoeInventoryTableModel
     {
         public string shoeName { get; set; }
-
         public string shoeDescription { get; set; }
-
         public string shoePrice { get; set; }
-
         public int shoeSize { get; set; }
-
         public int quantity { get; set; }
-
-
-    }//end-class
-}//end-namespace
+    }
+}
